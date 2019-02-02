35d70a86cc8502bb5796315a5e62b2277ca0a579
Zachary Ferretti <zf66728@gmail.com>
2019-02-02 15:08:47 +0000

    Navbar Updated
    Re-stylized the navbar, now i am going to add the css for mobile.

--- MSYInitial/navscroll.js
+++ MSYInitial/navscroll.js
@@ -4,14 +4,8 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 120 || document.documentElement.scrollTop > 120) {
-
     document.getElementById("navbar").style.visibility = "visible";
-
-
   } else {
-
-    document.getElementById("navbar").style.visibility = "hidden";
-    //document.getElementById("navbar").style.opacity = "0.5";
     document.getElementById("logo").style.visibility = "visible";
     document.getElementById("logobox").style.visibility = "visible";
 
@@ -20,13 +14,16 @@ function scrollFunction() {
     document.getElementById("navM").style.visibility = "visible"; 
     document.getElementById("navA").style.visibility = "visible"; 
     document.getElementById("navS").style.visibility = "visible";  
-
-    /*document.getElementById("navP").style.opacity = "1";
-    document.getElementById("navL").style.opacity = "1";
-    document.getElementById("navM").style.opacity = "1";
-    document.getElementById("navA").style.opacity = "1";
-    document.getElementById("navS").style.opacity = "1";*/
   }
 }
 
 
+function updateDisplay(){
+    var currentDisplay = document.getElementById('theNavbar');
+    if(navbar.className === 'navbar'){
+        currentDisplay.className += 'current'
+    }
+    else{
+        currentDisplay.className = 'navbar'
+    } 
+}

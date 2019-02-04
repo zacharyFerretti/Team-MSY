5a51c937c175ac7c39cadf4ddc95f7240c7d48f5
Zachary Ferretti <zf66728@gmail.com>
2019-02-04 10:04:40 +0000

    Working on JS Slideshow Error
    For some reason, the javascript code I wrote, although syntactically correct does not properly update the display style of the slideshow image which should be on display at any given moment.

--- MSYInitial/navscroll.js
+++ MSYInitial/navscroll.js
@@ -27,3 +27,36 @@ function updateDisplay(){
         currentDisplay.className = 'navbar'
     } 
 }
+
+var currSlide = 1;
+slidePresented(currSlide);
+
+function updateSlides(incrementor){
+    currSlide = currSlide + incrementor;
+    slidePresented(currSlide);
+}
+function goToSlide(incrementor){
+    currSlide = incrementor;
+    slidePresented(currSlide);
+}
+function slidePresented(incrementor){
+    var circlePosition = document.getElementByClassName("slideCircle");
+
+    var slides = document.getElementByClassName("slide");
+
+    var index;
+
+    if(incrementor > slides.length) {
+        currSlide=1;
+    }if(incrementor < 1){
+        currSlide = slides.length;
+    }
+    for(i = 0; i<slidse.length; i++){
+        slides[i].style.display = "none";
+    }
+    for(i = 0; i<circlePosition.length; i++){
+        circlePosition[i].className = circlePosition[i].className.replace(" active", "");
+    }
+    slides[currSlide-1].style.display = "block";
+    circlePosition[currSlide-1].className += " active";
+}

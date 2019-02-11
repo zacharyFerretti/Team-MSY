eef0151a4e941a0e8b9c9029b8cfee192693a7cd
Zachary Ferretti <zf66728@gmail.com>
2019-02-11 10:25:59 +0000

    Updated MSY Initial

--- MSYInitial/navscroll.js
+++ MSYInitial/navscroll.js
@@ -1,6 +1,5 @@
+//JS For Fixed Scrolling
 window.onscroll = function() {scrollFunction()};
-
-
 
 function scrollFunction() {
   if (document.body.scrollTop > 120 || document.documentElement.scrollTop > 120) {
@@ -16,7 +15,42 @@ function scrollFunction() {
     document.getElementById("navS").style.visibility = "visible";  
   }
 }
+//Map JS
+function initMap(){
+  
+    var myLatLng = {lat: -25.363, lng: 131.044};
 
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 4,
+      center: myLatLng
+    });
+    
+
+     new google.maps.Marker({
+      position: {lat:-26.363, lng:130.044},
+      map: map,
+      title: 'Hello World!'
+    });
+
+    function newMarker(lt, lg){
+
+      new google.maps.Marker({
+        position: {lat: lt, lng: lg},
+        map: map,
+        title: 'shop'
+    });
+  }
+  newMarker(-27.65, 153.15)
+  newMarker(-33.96365, 151.093827)
+  newMarker(-35.330400, 149.179830)
+  newMarker(-38.151700, 144.359300)
+  newMarker(-42.833519, 147.262955)
+  newMarker(-34.958591, 138.544266)
+  newMarker(-32.014710, 115.926980)
+  
+}
+
+//JS For Product Scrolling
 prodContainer.onscroll = function() {itemScrollFunction()};
 
 function itemScrollFunction(){
@@ -28,6 +62,8 @@ function itemScrollFunction(){
     }
 }
 
+
+//Basketbar JS
 var basketOpen = false;
 
 function expandBasket(){
@@ -87,7 +123,7 @@ function addToBasket(name, price, src){
 }
 
 
-
+//Slideshow JS
 function updateDisplay(){
     var currentDisplay = document.getElementById('theNavbar');
     if(navbar.className === 'navbar'){
